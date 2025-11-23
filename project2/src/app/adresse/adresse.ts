86c8a9d56afe7abd9ffa5ba81e52c65f8dc43bae
Yesser-Belhajali <medyesserbha@gmail.com>
2025-11-23 18:36:27 +0100

    Delete project2 directory

--- project2/src/app/adresse/adresse.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-    selector: 'app-adresse',
-    templateUrl: './adresse.html',
-    styleUrls: ['./adresse.css']
-})
-export class adresse {
-    rue = '123 Rue Exemple';
-}
