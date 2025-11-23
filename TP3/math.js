9dd92284b44cf8a49e409b93ac999a05603d375a
Yesser-Belhajali <medyesserbha@gmail.com>
2025-11-23 18:25:18 +0100

    Add files via upload

---
+++ TP3/math.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add1 = add1;
+exports.subtract = subtract;
+//Exercice8
+//question1
+function add1(a, b) {
+    return a + b;
+}
+//question3
+function subtract(a, b) {
+    return a - b;
+}
---
+++ TP3/math.ts
@@ -0,0 +1,9 @@
+//Exercice8
+//question1
+export function add1(a: number, b: number): number {
+  return a + b;
+}
+//question3
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
